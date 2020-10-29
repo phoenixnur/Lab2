acaea4da96010c85830a62e53cd4043c54ba050f
Nur <2020975359@isiswa.uitm.edu.my>
2020-10-29 04:19:58 +0000

    Commit for 2.1 Extra

--- 2.1.extra.c
+++ 2.1.extra.c
@@ -1,9 +1,8 @@
-  GNU nano 4.8                         2.1.c
 #include <stdio.h>
 #include <sys/types.h>
 /* #include <unistd.h> */
 
-int main(){
+int main() {
 fork();
 printf("Hello World\n");
 return 0;
